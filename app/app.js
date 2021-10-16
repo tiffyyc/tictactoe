cbc7fea7e32c96d6c1ec5324dd49f05dd88a114c
tiffyyc <yuenyuecheng@gmail.com>
2021-10-16 13:01:45 -0700

    added winning conditions

--- app/app.js
+++ app/app.js
@@ -42,7 +42,7 @@ $(() => {
 			currentPlayer = 'X'
 		}
 		//print on the page
-		$('.player').innerHTML = alternateTurn()
+		$('.players').innerHTML = alternateTurn()
 	}
 
 	//when clicking on one of the boxes, show the symbol of the current player
@@ -51,7 +51,7 @@ $(() => {
 		gameCellArray[boxIndex] = currentPlayer
 		// calling the UI to print x or o
 		clickedBox.innerHTML = currentPlayer
-	}
+	} 
 
 	// event listener
 	// click on each box
@@ -62,11 +62,67 @@ $(() => {
 		const boxIndex = parseInt(
 			clickedBox.getAttribute('data-cell-index')
 			)
-			/* // if the box is filled or the game ended
-			if (gameCellArray[boxIndex] !== '' || !gameActive) {
-				// player cannot click anymore
+			// if the box is filled or the game ended
+			if (gameCellArray[boxIndex] === boxIndex || gameActive) {
+				onBoxClicked(clickedBox, boxIndex)
+				alternatePlayer()
+				checkWinner()	
+			} 
+			console.log('player clicked')
 			
-			} else */
-			// otherwise, player can keep playing the game
-			onBoxClicked(clickedBox, boxIndex)
 	})
+
+	//winning conditions
+	// to determine winners, player has to have three of the 'x' or 'o' displayed
+	// either horizontally, vertically or diagonally
+	// using the index of gameCellArray
+	 const winningConditions = [
+		 //horizontally
+		 [0,1,2],
+		 [3,4,5],
+		 [6,7,8],
+		 //vertically
+		 [0,3,6],
+		 [1,4,7],
+		 [2,5,8],
+		 //diagonally
+		 [0,4,8],
+		 [2,4,6]
+	 ]
+
+	 // create a function to check if the conditions are met
+	 function checkWinner () {
+		 let playerWon = false
+		 // create a for loop to loop all conditions
+		 for (let i = 0; i <= winningConditions.length; i++) {
+			 //check if the clickebBox met the condition of the 'winningConditions' variable
+			 const winningCondition = winningConditions[i]
+			 // if cell are empty, game is not over
+			 if (gameCellArray[winningCondition[0]] === '' || gameCellArray[winningCondition[1]] === '' || gameCellArray[winningCondition[2]] === '') {
+				 // game continue
+				 continue
+			 }
+			 // if cell 0 is clicked, check cell 0 equals cell 1 and cell 1 equals cell 2
+			 if (gameCellArray[winningCondition[0]] === gameCellArray[winningCondition[1]] &&
+				 gameCellArray[winningCondition[1]] === gameCellArray[winningCondition[2]]) {
+					 playerWon = true
+					 //display winning message
+					 $('.players').innerHTML = winnerMessage
+					 // game is OVER
+					 gameActive = false
+					 // break the loop
+					 break
+				 }
+			} 
+				
+				// if the game is tied
+				// if all boxes are filled, there are no empty cell array
+				const playerTied = !gameCellArray.includes('')
+				if (playerTied) {
+					// player cannot click anymore
+					$('.players').innerHTML = tieGame
+					gameActive = false
+				}  
+				alternatePlayer()
+			}
+			   
